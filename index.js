966aebb530724a366e5812f0d4a0d7b243a53621
Austin McCollom <austinmccollom@Austins-Air.attlocal.net>
2020-07-15 17:48:03 -0500

    task 6 and 7

--- index.js
+++ index.js
@@ -140,13 +140,16 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(/*code here*/){
-
-    /*code here*/
-
+function copy(oldArray, newArray){
+    newArray = [...oldArray];
+    return newArray;
 }
+console.log(copy(originalFlavors, "mutableOriginals"));
 
-/* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
+/* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. 
+Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather 
+than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also 
+filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
 Your function should accept: 
 
@@ -161,12 +164,17 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
-
-    /*code here*/
-
+function filterByWord(array, string){
+    let filteredArray = [];
+    for ( i = 0; i < array.length ; i++ ) {
+        if (array[i].toLowerCase().includes(string.toLowerCase())) {
+            filteredArray.push(array[i]);
+        }
+    }
+    return filteredArray;
 }
 
+console.log(filterByWord(originalFlavors, 'chocolate'))
 
 
 /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/ 
