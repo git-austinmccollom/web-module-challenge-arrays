d1174661f9b07ba0715c91cd1779a9d905068e0c
Austin McCollom <austinmccollom@Austins-Air.attlocal.net>
2020-07-15 16:31:40 -0500

    completed tasks 1-3

--- index.js
+++ index.js
@@ -40,11 +40,14 @@ To save you from having to count the items above, you can assume that length of 
 
 i.e. is31Flavors(originalFlavors) will return TRUE.*/
 
-function is31Flavors(/*code here*/){
-
-    /*code here*/
-
+function is31Flavors(arr) {
+    if ( arr.length === 31 ) {
+        return true;
+    } else {
+        return false;
+    }
 }
+console.log(is31Flavors(originalFlavors));
 
 /* Task 2: Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
 
@@ -57,12 +60,15 @@ Your function should add the flavor to the front of the array and console.log th
 
 For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"] */ 
 
-function addFlavor(/*code here*/){
-
-    /*code here*/
-
+function addFlavor(flavor, array){
+    let flavorAddedArray = [...array];
+    flavorAddedArray.unshift(flavor);
+    console.log(flavorAddedArray);
+    return flavorAddedArray;
 }
 
+flavorsTwo = addFlavor('Rainbow Sherbert', originalFlavors);
+console.log(flavorsTwo);
 
 /* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item from the end of the array. 
 
@@ -74,11 +80,14 @@ Your function should remove a flavor from the end of the array and console.log t
 
 For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/ 
 
-function removeLastFlavor(/*code here*/){
-
-    /*code here*/
-
+function removeLastFlavor(array){
+    let flavorRemovedArray = [...array];
+    flavorRemovedArray.pop();
+    return flavorRemovedArray;
 }
+
+flavorsThree = removeLastFlavor(flavorsTwo);
+console.log(flavorsThree);
 
 /* Task 4: Write a function that returns a flavor at a given index in the array.
 
